d0f5b92582592bf3df2e402d4222c5cbc0277973
flavia-1702 <167451562+flavia-1702@users.noreply.github.com>
2024-04-20 19:41:32 -0300

    Add files via upload

---
+++ Strings/inversao_de_uma_string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(){
+    string s, aux;
+    int n, a, b, i;
+
+    cin >> n >> a >> b;
+    cin >> s;
+
+    for(i = 0; i < n; i++)
+        aux = s;
+    
+    i = b-1;
+
+    for(int j = a-1; j <= b-1; j++){
+        aux[j] = s[i];    // aux vai receber os caracteres invertido da string s(apenas os índices de "a" até "b")
+        i--;
+    }
+
+    s = aux;
+
+    cout << aux << "\n";
+
+    return 0;
+}
