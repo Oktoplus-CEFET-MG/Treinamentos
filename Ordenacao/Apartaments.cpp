8e464452d3dcd807b03a06926a4c528387a78d12
WendellReis <79986893+WendellReis@users.noreply.github.com>
2024-05-24 16:54:00 -0300

    Exercicio Apartaments (CSES)

---
+++ Ordenacao/Apartaments.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n,m,k; cin >> n >> m >> k;
+ 
+    vector<int> ap(n),apt(m);
+    for(int i = 0; i < n; i++)
+        cin >> ap[i];
+    for(int i = 0; i < m; i++)
+        cin >> apt[i];
+ 
+    sort(ap.begin(),ap.end());
+    sort(apt.begin(),apt.end());
+ 
+    int i = 0, j = 0, count = 0;
+    while(i < n && j < m) {
+        if(abs(ap[i] - apt[j]) <= k) {
+            i++;
+            j++;
+            count++;
+        } else if(ap[i] < apt[j])
+            i++;
+        else
+            j++;
+    }
+ 
+    cout << count << "\n";
+    return 0;
+}
